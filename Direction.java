ab4f2db4b758ae924f9ffb092a3a5f696102ed4c
CYW <chenyuw2019@gmail.com>
2025-11-02 19:47:51 -0500

    Answred 43 by defining my own class called Direction

---
+++ Direction.java
@@ -0,0 +1,12 @@
+
+/**
+ * The Direction enum has all the possible movement directions. Because we have legs.
+ * Legs are good.
+ * Legs are great.
+ * ALL HAIL LEGS!
+ * 
+ */
+public enum Direction
+{
+    NORTH, SOUTH, EAST, WEST
+}
